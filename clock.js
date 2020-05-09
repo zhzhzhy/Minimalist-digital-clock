ec43f43e766fec7554a1edb18884294bba2cf9cf
Geek_cat <by6dx1@gmail.com>
2020-05-09 22:55:41 +0800

    Make it more perfect (clock.js)

--- clock.js
+++ clock.js
@@ -12,12 +12,13 @@ function update(){
 	/* ------Define End------*/
 
 	var WeekName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];   //Store WeekName parameters
+	var MonthName = ["January","February","March","April","May","June","July","August","September","October","November","December"]; //Store MonthName parameters
 
 	//The result to be printing(If the Month,Date,Hour,Minutes,Seconds are less than 10,add additional "0" String)
 	var Realtime = (hour<10?"0"+hour:hour) + ":" + (minutes<10?"0"+minutes:minutes) + ":" + (seconds<10?"0"+seconds:seconds);  
-	var ToDay = year + "-" + ((month+1)>10?(month+1):"0"+(month+1)) + "-" + (day>10?day:"0"+day);
+	var ToDay = year + " " + MonthName[month] + " " + (day>10?day:"0"+day);
 
-	document.getElementById("timer").innerHTML = ToDay + "\r" + Realtime + "\r" + WeekName[week]; //Print the result
+	document.getElementById("timer").innerHTML = ToDay + "<br>" + Realtime + "<br>" + WeekName[week]; //Print the result
 }
 
 setInterval(update,500);
